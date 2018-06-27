84fc91476e055c20ec1979dcf484d7476b2c6985
ashikov <6546@mail.ru>
2018-06-27 18:01:47 +0300

    Self route v.1.2

--- src/Service/Containers/HeaderContainer.js
+++ src/Service/Containers/HeaderContainer.js
@@ -8,23 +8,26 @@ import createHashHistory from "history/es/createHashHistory";
 import {SetPage} from "../Actions/SetPage";
 
 class HeaderContainer extends Component {
-    checkHistory(){
+    checkHistory(e){
         this.props.setHistory(null);
         this.props.setPage("history");
+        e.preventDefault();
     }
-    checkHome(){
+    checkHome(e){
         this.props.setPage("");
+        e.preventDefault();
     }
-    checkInfo(){
+    checkInfo(e){
         this.props.setPage("info");
+        e.preventDefault();
     }
     render() {
         return (
                 <div className="header">
                     <ul>
-                        <li><a href="#" className={this.props.page === "" ? "active" : ""} onClick={this.checkHome.bind(this)}>ИГРА. Количество ставок: <b>{this.props.data.attempts}</b></a></li>
-                        <li><a href="#" className={this.props.page === "history" ? "active" : ""} onClick={this.checkHistory.bind(this)}>История ставок</a></li>
-                        <li><a href="#" className={this.props.page === "info" ? "active" : ""} onClick={this.checkInfo.bind(this)}>Информация</a></li>
+                        <li><a href="" className={this.props.page === "" ? "active" : ""} onClick={this.checkHome.bind(this)}>ИГРА. Количество ставок: <b>{this.props.data.attempts}</b></a></li>
+                        <li><a href="" className={this.props.page === "history" ? "active" : ""} onClick={this.checkHistory.bind(this)}>История ставок</a></li>
+                        <li><a href="" className={this.props.page === "info" ? "active" : ""} onClick={this.checkInfo.bind(this)}>Информация</a></li>
                     </ul>
                 </div>
         )
