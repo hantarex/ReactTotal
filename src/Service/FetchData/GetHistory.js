21754533ebe71617a440f76f9d0e8e1ebbf458a5
ashikov <6546@mail.ru>
2018-06-28 16:38:48 +0300

    Self route v.1.4

--- src/Service/FetchData/GetHistory.js
+++ src/Service/FetchData/GetHistory.js
@@ -3,76 +3,76 @@ import {HistoryLoadingActions} from "../Actions/HistoryLoadingAction";
 import axios from 'axios';
 
 export default function() {
-    let data = {
-        bets: [
-            {
-                id: 1,
-                date: "30.06",
-                time: "19:00",
-                check: "1231",
-                teams:{
-                    4:{
-                        name: "Россия",
-                        flag: "flag-ru"
-                    },
-                    5:{
-                        name: "Бразилия",
-                        flag: "flag-br"
-                    },
-                },
-                bet: {
-                    bets_type: 3,
-                    value: {
-                        4: 12,
-                        5: 1,
-                    }
-                },
-                result: "В ожидании матча"
-            },
-            {
-                id: 2,
-                date: "30.06",
-                time: "19:00",
-                check: "1231",
-                teams:{
-                    4:{
-                        name: "Россия",
-                        flag: "flag-ru"
-                    },
-                    5:{
-                        name: "Бразилия",
-                        flag: "flag-br"
-                    },
-                },
-                bet: {
-                    bets_type: 2,
-                    value: 45
-                },
-                result: "Нет"
-            },
-            {
-                id: 1,
-                date: "30.06",
-                time: "19:00",
-                check: "1231",
-                teams:{
-                    4:{
-                        name: "Россия",
-                        flag: "flag-ru"
-                    },
-                    5:{
-                        name: "Бразилия",
-                        flag: "flag-br"
-                    },
-                },
-                bet: {
-                    bets_type: 1,
-                    value: 4
-                },
-                result: "Да"
-            },
-        ]
-    };
+    // let data = {
+    //     bets: [
+    //         {
+    //             id: 1,
+    //             date: "30.06",
+    //             time: "19:00",
+    //             check: "1231",
+    //             teams:{
+    //                 4:{
+    //                     name: "Россия",
+    //                     flag: "flag-ru"
+    //                 },
+    //                 5:{
+    //                     name: "Бразилия",
+    //                     flag: "flag-br"
+    //                 },
+    //             },
+    //             bet: {
+    //                 bets_type: 3,
+    //                 value: {
+    //                     4: 12,
+    //                     5: 1,
+    //                 }
+    //             },
+    //             result: "В ожидании матча"
+    //         },
+    //         {
+    //             id: 2,
+    //             date: "30.06",
+    //             time: "19:00",
+    //             check: "1231",
+    //             teams:{
+    //                 4:{
+    //                     name: "Россия",
+    //                     flag: "flag-ru"
+    //                 },
+    //                 5:{
+    //                     name: "Бразилия",
+    //                     flag: "flag-br"
+    //                 },
+    //             },
+    //             bet: {
+    //                 bets_type: 2,
+    //                 value: 45
+    //             },
+    //             result: "Нет"
+    //         },
+    //         {
+    //             id: 1,
+    //             date: "30.06",
+    //             time: "19:00",
+    //             check: "1231",
+    //             teams:{
+    //                 4:{
+    //                     name: "Россия",
+    //                     flag: "flag-ru"
+    //                 },
+    //                 5:{
+    //                     name: "Бразилия",
+    //                     flag: "flag-br"
+    //                 },
+    //             },
+    //             bet: {
+    //                 bets_type: 1,
+    //                 value: 4
+    //             },
+    //             result: "Да"
+    //         },
+    //     ]
+    // };
     // We return a function instead of an action object
     // return (dispatch) => {
     //     setTimeout(() => {
